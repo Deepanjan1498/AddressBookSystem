da942e6e3fcbd1ae67176e6045f4f0d52f14625b
Deepanjan1498 <deepanjansharma43@gmail.com>
2020-10-10 04:49:37 +0530

    [Deepanjan]Add . uc5 is solved to provide ability to add multiple person

--- src/capgemini/addressbooksystem/AddressBookMain.java
+++ src/capgemini/addressbooksystem/AddressBookMain.java
@@ -6,69 +6,19 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class AddressBookMain {
-	
 
-	Scanner sc=new Scanner(System.in);
 	private ArrayList<Contacts> contactArray;
-	private Map<String,Contacts> contactMap;
-	public AddressBookMain()
-	{
-		contactArray=new ArrayList<>();
-		contactMap=new HashMap<>();
+	private Map<String, Contacts> contactMap;
+
+	public AddressBookMain() {
+		contactArray = new ArrayList<>();
+		contactMap = new HashMap<>();
 	}
+
 	/**
-	 *uc2
+	 * uc2
 	 */
-	public void addNewContact(String firstName, String lastName, long phoneNumber, String emailId, String address,
-			int zipcode, String city, String state) {
-		Contacts con = new Contacts(firstName, lastName, phoneNumber, emailId, address, zipcode, city,
-				state);
-		contactArray.add(con);
-		contactMap.put(firstName,con);
-	}
-	/**
-	 * uc3
-	 */
-	public void editContact()
-	{
-		System.out.println("Enter The First Name Whose contact details is to be Edited: ");
-		String fn=sc.nextLine();
-		Contacts obj=contactMap.get(fn);
-		System.out.println();
-		System.out.println("Enter the PhoneNumber");
-		int num=sc.nextInt();
-		obj.setPhoneNumber(num);
-		System.out.println("Enter the Email");
-		String emId=sc.next();
-		obj.setEmailId(emId);
-		System.out.println("Enter the Address");
-		String add=sc.next();
-		obj.setAddress(add);
-		System.out.println("Enter the Zipcode");
-		int zip=sc.nextInt();
-		obj.setZipCode(zip);
-		System.out.println("Enter the City");
-		String cit=sc.next();
-		obj.setCity(cit);
-		System.out.println("Enter the State");
-		String sta=sc.next();
-		obj.setState(sta);
-	}
-	public void printContact()
-	{
-	System.out.println(contactArray);
-	}
-	/**
-	 * uc4
-	 */
-	public void deleteContact()
-	{
-		System.out.println("Enter The First Name to delete the contact details");
-		String fn=sc.nextLine();
-		Contacts obj=contactMap.get(fn);
-		contactArray.remove(obj);
-	}
-	public static void main(String args[]) {
+	public void addNewContact() {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Enter First Name of the contact");
 		String firstName = s.nextLine();
@@ -86,15 +36,72 @@ public class AddressBookMain {
 		String city = s.next();
 		System.out.println("Enter State of the contact");
 		String state = s.next();
+		Contacts con = new Contacts(firstName, lastName, phoneNumber, emailId, address, zipcode, city, state);
+		contactArray.add(con);
+		contactMap.put(firstName, con);
+	}
+
+	/**
+	 * uc3
+	 */
+	public void editContact() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter The First Name Whose contact details is to be Edited: ");
+		String fn = sc.nextLine();
+		Contacts obj = contactMap.get(fn);
+		System.out.println();
+		System.out.println("Enter the PhoneNumber");
+		int num = sc.nextInt();
+		obj.setPhoneNumber(num);
+		System.out.println("Enter the Email");
+		String emId = sc.next();
+		obj.setEmailId(emId);
+		System.out.println("Enter the Address");
+		String add = sc.next();
+		obj.setAddress(add);
+		System.out.println("Enter the Zipcode");
+		int zip = sc.nextInt();
+		obj.setZipCode(zip);
+		System.out.println("Enter the City");
+		String cit = sc.next();
+		obj.setCity(cit);
+		System.out.println("Enter the State");
+		String sta = sc.next();
+		obj.setState(sta);
+	}
+
+	public void printContact() {
+		System.out.println(contactArray);
+	}
+
+	/**
+	 * uc4
+	 */
+	public void deleteContact() {
+		Scanner sd = new Scanner(System.in);
+		System.out.println("Enter The First Name to delete the contact details");
+		String first = sd.nextLine();
+		Contacts obj = contactMap.get(first);
+		contactArray.remove(obj);
+	}
+
+	/**
+	 * uc5
+	 *args
+	 */
+	public static void main(String args[]) {
+		Scanner sa = new Scanner(System.in);
 		AddressBookMain Contactno1 = new AddressBookMain();
-		Contactno1.addNewContact(firstName, lastName, phoneNumber, emailId, address, zipcode, city, state);
-        Contactno1.printContact();
-        Contactno1.editContact();
-        System.out.println("After Editing the conntacts :");
-        Contactno1.printContact();
-        Contactno1.deleteContact();
-        System.out.println("After Deleting the contact:");
-        Contactno1.printContact();
+		for (int i = 0; i >= 0; i++) {
+			System.out.println("Do You Want to add a Contact?(Y/N)");
+			char choice = sa.next().charAt(0);
+			if (choice == 'Y') {
+				Contactno1.addNewContact();
+				Contactno1.printContact();
+			} else
+				System.exit(0);
+
+		}
 	}
 
 }
